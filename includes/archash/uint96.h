c75bc41198dd9bf159c9ac140a771e5d414e40ba
lamadaemon <145078438+lamadaemon@users.noreply.github.com>
2024-01-09 09:18:06 -0800

    add uint96_t

---
+++ includes/archash/uint96.h
@@ -0,0 +1,27 @@
+//
+// Created by lamadaemon on 11/30/2023.
+//
+// Special thanks to @7aGiven
+//
+
+#ifndef NARCHOOK_UIN96_H
+#define NARCHOOK_UIN96_H
+
+#include <cstdint>
+
+namespace narchook::utils {
+
+    typedef struct uint96 {   
+        uint64_t low;
+        uint32_t high;
+    } uint96_t;
+
+    uint96_t u96_and(uint96_t* a, uint96_t* b);
+    uint96_t u96_or(uint96_t* a, uint96_t* b);
+    uint96_t u96_not(uint96_t* a, uint96_t* b);
+    uint96_t u96_xor(uint96_t* a, uint96_t* b);
+    uint96_t u96_rtol(uint96_t* b, uint32_t n);
+
+
+}
+#endif//NARCHOOK_UINT96_H
---
+++ jni/archash/uint96.cpp
@@ -0,0 +1,48 @@
+#include "archash/uint96.h"
+
+namespace narchook::utils {
+
+    uint96_t u96_and(uint96_t* a, uint96_t* b) {
+        uint96_t result;
+        result.low = a->low & b->low;
+        result.high = a->high & b-> high;
+        return result;
+    }
+
+    uint96_t u96_or(uint96_t* a, uint96_t* b) {
+        uint96_t result;
+        result.low = a->low | b->low;
+        result.high = a->high | b-> high;
+        return result;
+    }
+
+    uint96_t u96_not(uint96_t* a) {
+        uint96_t result;
+        result.low = ~a->low;
+        result.high = ~a->high;
+        return result;
+    }
+
+    uint96_t u96_xor(uint96_t* a, uint96_t* b) {
+        uint96_t result;
+        result.low = a->low ^ b->low;
+        result.high = a->high ^ b-> high;
+        return result;
+    }
+
+    uint96_t u96_rtol(uint96_t* a, uint32_t n) {
+        uint96_t result;
+
+        while (n > 32) {
+            result.high = a->low >> 32;
+            a->low = a->low << 32 | a->high;
+            a->high = result.high;
+            n -= 32;
+        }
+     
+        result.low = a->low << n | a->high >> (32 - n);
+        result.high = a->high << n | a->low >> (64 - n);
+    }
+
+
+}

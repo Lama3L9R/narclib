a9e918b094d5313a732610a1345b587d0de6ccca
Qumolama <Guzhipei@outlook.com>
2024-01-31 00:51:05 -0800

    improve inline security

--- narchash/includes/uint96.h
+++ narchash/includes/uint96.h
@@ -8,6 +8,7 @@
 #define NARCHOOK_UIN96_H
 
 #include <cstdint>
+#include "narcutils.h"
 
 namespace narcutils::utils {
 
@@ -16,11 +17,49 @@ namespace narcutils::utils {
         uint32_t high;
     } uint96_t;
 
-    uint96_t u96_and(uint96_t a, uint96_t b);
-    uint96_t u96_or(uint96_t a, uint96_t b);
-    uint96_t u96_not(uint96_t a);
-    uint96_t u96_xor(uint96_t a, uint96_t b);
-    uint96_t u96_rtol(uint96_t b, uint32_t n);
+    uint96_t EncryptedAPI u96_and(uint96_t a, uint96_t b) {
+        uint96_t result;
+        result.low = a.low & b.low;
+        result.high = a.high & b. high;
+        return result;
+    }
+
+    uint96_t EncryptedAPI u96_or(uint96_t a, uint96_t b) {
+        uint96_t result;
+        result.low = a.low | b.low;
+        result.high = a.high | b. high;
+        return result;
+    }
+
+    uint96_t EncryptedAPI u96_not(uint96_t a) {
+        uint96_t result;
+        result.low = ~a.low;
+        result.high = ~a.high;
+        return result;
+    }
+
+    uint96_t EncryptedAPI u96_xor(uint96_t a, uint96_t b) {
+        uint96_t result;
+        result.low = a.low ^ b.low;
+        result.high = a.high ^ b. high;
+        return result;
+    }
+
+    uint96_t EncryptedAPI u96_rtol(uint96_t a, uint32_t n) {
+        uint96_t result;
+
+        while (n > 32) {
+            result.high = a.low >> 32;
+            a.low = a.low << 32 | a.high;
+            a.high = result.high;
+            n -= 32;
+        }
+
+        result.low = a.low << n | a.high >> (32 - n);
+        result.high = a.high << n | a.low >> (64 - n);
+
+        return result;
+    }
 
 
 }

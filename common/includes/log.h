19a67cf944f87f961337c3f5e938c7b00a9fd14e
Qumolama <Guzhipei@outlook.com>
2024-02-01 16:55:02 -0800

    implemented log functions in every platform

--- common/includes/log.h
+++ common/includes/log.h
@@ -20,11 +20,63 @@
 #endif
 
 #ifndef LOG_PROVIDED
+#include <cstdarg>
 
-#define LOGE(...)
-#define LOGW(...)
-#define LOGI(...)
-#define LOGD(...)
+#ifndef LOG_PROVIDED
+
+#include <cstdarg>
+#include <cstdio>
+
+
+#define LOGE narcutils::log::error
+#define LOGW narcutils::log::warn
+#define LOGI narcutils::log::info
+#define LOGD narcutils::log::debug
+
+namespace narcutils::log {
+    void EncryptedAPI vlog(const char tag[], const char format[], va_list args) {
+#ifdef NARCHOOK_BUILD_DEBUG
+        size_t final_format_len = strlen(tag) + 3 + strlen(format) + 1 + 1;
+        char* final_format_buff = (char*) malloc(final_format_len);
+
+        snprintf(final_format_buff, final_format_len, "[%s] %s\n", tag, format);
+        vprintf(final_format_buff, args);
+
+        free(final_format_buff);
+#endif
+    }
+
+    void EncryptedAPI debug(const char format[], ...) {
+        va_list args;
+        va_start(args, format);
+        vlog("narchash::debug", format, args);
+        va_end(args);
+    }
+
+    void EncryptedAPI info(const char format[], ...) {
+        va_list args;
+        va_start(args, format);
+        vlog("narchash::info", format, args);
+        va_end(args);
+    }
+
+    void EncryptedAPI warn(const char format[], ...) {
+        va_list args;
+        va_start(args, format);
+        vlog("narchash::warn", format, args);
+        va_end(args);
+    }
+
+    void EncryptedAPI error(const char format[], ...) {
+        va_list args;
+        va_start(args, format);
+        vlog("narchash::error", format, args);
+        va_end(args);
+    }
+}
+#endif
+
+
 
 #endif
 
